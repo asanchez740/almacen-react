db52c2dfc59233428f1fd957804a33f0477ae1df
asanchez740 <argelsanchez19@gmail.com>
2026-05-20 15:15:53 -0300

    dasboard test y navegacion con router

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
+
+
+export default function Dashboard() {
+  const navigate = useNavigate();
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold mb-6">
+        Dashboard
+      </h1>
+
+      <div className="flex gap-4">
+
+        <button
+            onClick={() => navigate("/almacen")}
+            className="bg-blue-600 text-white px-6 py-4 rounded-xl"
+        >
+          Ir al Almacén
+       </button>
+
+      </div>
+    </div>
+  );
+}
